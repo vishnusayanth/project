d1edf3e807715dfcc9317bc501d8ab44f38b5558
Vishnu Sayanth A V <vishnusayanth@gmail.com>
2024-04-26 12:03:50 +0530

    Update Nasa.js

--- src/main/Nasa.js
+++ src/main/Nasa.js
@@ -40,128 +40,131 @@ export default function Nasa(props) {
             <small><i><b>It could take a few seconds to load the images.</b></i></small>
         </div>
         <div className="d-flex justify-content-around">
-        <div className="col-xxl-3 col-md-5">
-            <div className="card info-card sales-card">
-                <div className="card-body">
-                    <h5 className="card-title">Astronomy Pic <span>| Today</span></h5>
+            <div className="col-xxl-3 col-md-5">
+                <div className="card info-card sales-card">
+                    <div className="card-body">
+                        <h5 className="card-title">Astronomy Pic <span>| Today</span></h5>
 
-                    <div className="d-flex align-items-center">
-                        <div
-                            className="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                            <i className="bi bi-stars"></i>
+                        <div className="d-flex align-items-center">
+                            <div
+                                className="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                <i className="bi bi-stars"></i>
+                            </div>
+                            <div className="ps-3">
+                                <button type="button" className="btn btn-outline-primary" data-bs-toggle="modal"
+                                    data-bs-target="#picofday">
+                                    View picture of the day
+                                </button>
+                                <div className="modal fade" id="picofday" tabIndex="-1">
+                                    <div className="modal-dialog modal-dialog-centered">
+                                        {picOfDay && <div className="modal-content">
+                                            <div className="modal-header">
+                                                <h5 className="modal-title">{picOfDay.title}</h5>&nbsp;&nbsp;&nbsp;
+                                                <small>{picOfDay.date}</small>
+                                                <button type="button" className="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            {picOfDay &&
+                                                <div className="modal-body">
+
+                                                    {picOfDay.media_type === 'image' &&
+                                                        <img src={picOfDay.url} className={"img-fluid"}
+                                                            alt={"pic of day"} />}
+                                                    {picOfDay.media_type === 'video' &&
+                                                        <iframe width="420" height="315" className={"img-fluid"}
+                                                            src={picOfDay.url}></iframe>
+                                                    }
+                                                    <div className="overflow-auto" style={{ 'height': 300 }}>
+                                                        {picOfDay.explanation}
+                                                    </div>
+                                                </div>}
+                                        </div>}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="ps-3">
-                            <button type="button" className="btn btn-outline-primary" data-bs-toggle="modal"
-                                data-bs-target="#picofday">
-                                View picture of the day
-                            </button>
-                            <div className="modal fade" id="picofday" tabIndex="-1">
+                    </div>
+
+                </div>
+            </div>
+            <div className="col-xxl-3 col-md-5">
+                <div className="card info-card sales-card">
+                    <div className="card-body">
+                        <h5 className="card-title">Curiosity Rover <span>| refresh for new images</span></h5>
+
+                        <div className="d-flex align-items-center">
+                            <div
+                                className="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                <i className="bi bi-camera"></i>
+                            </div>
+                            <div className="ps-3">
+                                <button type="button" className="btn btn btn-outline-danger" data-bs-toggle="modal"
+                                    data-bs-target="#rover">
+                                    View mars snapshots
+                                </button>
+
+
+                            </div>
+                            <div className="modal fade" id="rover" tabIndex="-1">
                                 <div className="modal-dialog modal-dialog-centered">
-                                    {picOfDay && <div className="modal-content">
+
+                                    {roverPics && <div className="modal-content">
                                         <div className="modal-header">
-                                            <h5 className="modal-title">{picOfDay.title}</h5>&nbsp;&nbsp;&nbsp;
-                                            <small>{picOfDay.date}</small>
+                                            <h6 className="modal-title">Mars Snapshots</h6>&nbsp;&nbsp;&nbsp;&nbsp;
+                                            <small>({roverPics.length})</small>
                                             <button type="button" className="btn-close" data-bs-dismiss="modal"
                                                 aria-label="Close"></button>
                                         </div>
-                                        {picOfDay &&
-                                            <div className="modal-body">
-
-                                                {picOfDay.media_type === 'image' &&
-                                                    <img src={picOfDay.url} className={"img-fluid"}
-                                                        alt={"pic of day"} />}
-                                                {picOfDay.media_type === 'video' &&
-                                                    <iframe width="420" height="315" className={"img-fluid"}
-                                                        src={picOfDay.url}></iframe>
-                                                }
-                                                <div className="overflow-auto" style={{ 'height': 300 }}>
-                                                    {picOfDay.explanation}
+                                        <div className="modal-body">
+                                            <div className="alert alert-warning">
+                                                <small><i><b>It could take a few seconds to load the images.</b></i></small>
+                                            </div>
+                                            <div id="carouselExampleCaptions" className="carousel slide carousel-fade"
+                                                data-bs-ride="carousel">
+                                                <div className="carousel-inner" id={"carousel"}>
+                                                    <div className="carousel-item active">
+                                                        <div className="spinner-border" role="status" id={"rover-spinner"}>
+                                                        </div>
+                                                    </div>
+                                                    {roverPics.map(pic => {
+                                                        return <div key={pic.id} className="carousel-item">
+                                                            <img src={pic.img_src} className="d-block w-100"
+                                                                alt="pic from rover" />
+                                                            <div className="carousel-caption d-none d-md-block">
+                                                                <h5>{pic.earth_date}</h5>
+                                                                <p>{pic.camera.full_name}</p>
+                                                                <p>{pic.rover.name}</p>
+                                                            </div>
+                                                        </div>
+                                                    })}
                                                 </div>
-                                            </div>}
+                                                <button className="carousel-control-prev" type="button"
+                                                    data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                                                    <span className="carousel-control-prev-icon"
+                                                        aria-hidden="true"></span>
+                                                    <span className="visually-hidden">Previous</span>
+                                                </button>
+                                                <button className="carousel-control-next" type="button"
+                                                    data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                                                    <span className="carousel-control-next-icon"
+                                                        aria-hidden="true"></span>
+                                                    <span className="visually-hidden">Next</span>
+                                                </button>
+
+
+                                            </div>
+                                        </div>
+                                        <div className="modal-footer">
+
+                                        </div>
                                     </div>}
                                 </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
-
             </div>
-        </div>
-        <div className="col-xxl-3 col-md-5">
-            <div className="card info-card sales-card">
-                <div className="card-body">
-                    <h5 className="card-title">Curiosity Rover <span>| refresh for new images</span></h5>
-
-                    <div className="d-flex align-items-center">
-                        <div
-                            className="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                            <i className="bi bi-camera"></i>
-                        </div>
-                        <div className="ps-3">
-                            <button type="button" className="btn btn btn-outline-danger" data-bs-toggle="modal"
-                                data-bs-target="#rover">
-                                View mars snapshots
-                            </button>
-
-
-                        </div>
-                        <div className="modal fade" id="rover" tabIndex="-1">
-                            <div className="modal-dialog modal-dialog-centered">
-
-                                {roverPics && <div className="modal-content">
-                                    <div className="modal-header">
-                                        <h6 className="modal-title">Mars Snapshots</h6>&nbsp;&nbsp;&nbsp;&nbsp;
-                                        <small>({roverPics.length})</small>
-                                        <button type="button" className="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                    </div>
-                                    <div className="modal-body">
-                                        <div id="carouselExampleCaptions" className="carousel slide carousel-fade"
-                                            data-bs-ride="carousel">
-                                            <div className="carousel-inner" id={"carousel"}>
-                                                <div className="carousel-item active">
-                                                    <div className="spinner-border" role="status" id={"rover-spinner"}>
-                                                    </div>
-                                                </div>
-                                                {roverPics.map(pic => {
-                                                    return <div key={pic.id} className="carousel-item">
-                                                        <img src={pic.img_src} className="d-block w-100"
-                                                            alt="pic from rover" />
-                                                        <div className="carousel-caption d-none d-md-block">
-                                                            <h5>{pic.earth_date}</h5>
-                                                            <p>{pic.camera.full_name}</p>
-                                                            <p>{pic.rover.name}</p>
-                                                        </div>
-                                                    </div>
-                                                })}
-                                            </div>
-                                            <button className="carousel-control-prev" type="button"
-                                                data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                                                <span className="carousel-control-prev-icon"
-                                                    aria-hidden="true"></span>
-                                                <span className="visually-hidden">Previous</span>
-                                            </button>
-                                            <button className="carousel-control-next" type="button"
-                                                data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                                                <span className="carousel-control-next-icon"
-                                                    aria-hidden="true"></span>
-                                                <span className="visually-hidden">Next</span>
-                                            </button>
-
-
-                                        </div>
-                                    </div>
-                                    <div className="modal-footer">
-
-                                    </div>
-                                </div>}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
         </div>
 
         <center>
